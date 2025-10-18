4db7907b75d12d227e7938b343b5f65e3f686bfb
martin433ai <parkmartin433@gmail.com>
2025-10-18 07:26:45 +0200

    Initial commit: ESP-IDF hello_world project

---
+++ test_pytapo.py
@@ -0,0 +1,24 @@
+"""
+Test file for pytapo functionality
+"""
+
+import unittest
+
+
+class TestPytapo(unittest.TestCase):
+    
+    def setUp(self):
+        """Set up test fixtures before each test method."""
+        pass
+    
+    def tearDown(self):
+        """Clean up after each test method."""
+        pass
+    
+    def test_example(self):
+        """Example test case."""
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
